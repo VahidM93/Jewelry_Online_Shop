479c09028e86380b3a45542b32d5c350c048bca6
VahidM93 <vahid9068@gmail.com>
2023-07-26 13:07:19 +0330

    models accounts created

--- Jewelry_Online_Shop/accounts/models.py
+++ Jewelry_Online_Shop/accounts/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from .managers import AccountManager
+from django.utils.translation import gettext_lazy as _
+from core.utils import phone_regex_validator
+from core.models import ModelInfo
 
-# Create your models here.
+
+class Account(AbstractBaseUser, PermissionsMixin):
+    full_name = models.CharField(max_length=100, verbose_name='Full name')
+    birth_date = models.DateField(null=True, blank=True, verbose_name='Birthday')
+    postal_code = models.IntegerField(null=True, blank=True, verbose_name='Postal Code')
+    phone_number = models.CharField(max_length=13, unique=True, validators=[phone_regex_validator],
+                                    verbose_name=_('Phone Number'))
+    email = models.EmailField(max_length=255, unique=True, null=True, blank=True, verbose_name='Email')
+    created_at=models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    objects = AccountManager()
+
+    class Meta:
+        verbose_name = _('Account')
+        verbose_name_plural = _('Accounts')
+
+    USERNAME_FIELD = 'phone_number'
+    REQUIRED_FIELDS = ['firstname', 'lastname']
+
+    def __str__(self):
+        return self.phone_number
+
+    @property
+    def is_staff(self):
+        return self.is_admin
+
+
+class Address(ModelInfo):
+    address = models.CharField(max_length=200)
+    account_id = models.ForeignKey(Account, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.address
