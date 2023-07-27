c8fefe36a605658d57c62df6c6bba7e8a3c20c9b
VahidM93 <vahid9068@gmail.com>
2023-07-27 20:42:46 +0330

    feat:message

--- Jewelry_Online_Shop/accounts/models.py
+++ Jewelry_Online_Shop/accounts/models.py
@@ -1,9 +1,12 @@
+from datetime import timedelta,datetime
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+import pytz
 from .managers import AccountManager
 from django.utils.translation import gettext_lazy as _
 from core.utils import phone_regex_validator
-from core.models import ModelInfo
+from . import OtpCode
+
 
 
 class Account(AbstractBaseUser, PermissionsMixin):
@@ -31,3 +34,34 @@ class Account(AbstractBaseUser, PermissionsMixin):
     @property
     def is_staff(self):
         return self.is_admin
+
+    class OtpCode(models.Model):
+        phone_number = models.CharField(max_length=13, validators=[phone_regex_validator], verbose_name=_('Phone Number'))
+        code = models.PositiveSmallIntegerField(verbose_name=_('Code'))
+        created = models.DateTimeField(auto_now=True, verbose_name=_('Created'))
+
+        class Meta:
+            verbose_name = _('Otp Code')
+            verbose_name_plural = _('Otp Codes')
+
+        def save(self, *args, **kwargs):
+            self.phone_number = '0' + self.phone_number[3:] if len(self.phone_number) == 13 else self.phone_number
+            super(OtpCode, self).save(*args, **kwargs)
+
+        @classmethod
+        def is_code_available(cls, phone_number):
+         counter = cls.objects.filter(phone_number=phone_number).count()
+         return False if counter > 5 else True
+
+        def __str__(self):
+            return f'{self.phone_number} - {self.code} - {self.created}'
+
+        def is_valid(self):
+            utc = pytz.UTC
+            expire = self.created + timedelta(minutes=32, hours=3)
+            checked_on = datetime.now().replace(tzinfo=utc)
+            expired_on = expire.replace(tzinfo=utc)
+            if expired_on > checked_on:
+                return True
+            self.delete()
+            return False
