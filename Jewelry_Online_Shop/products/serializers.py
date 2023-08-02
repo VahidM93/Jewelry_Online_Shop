592915f638a84344e17863423d7425ab602dfeef
VahidM93 <vahid9068@gmail.com>
2023-08-03 00:04:07 +0330

    update product serializers

--- Jewelry_Online_Shop/products/serializers.py
+++ Jewelry_Online_Shop/products/serializers.py
@@ -1,22 +1,44 @@
 from rest_framework import serializers
-from .models import Category, Property, Product, Comment
+from customers.serializers import CommentSerializer
+from .models import Category, Product, Comment
 
 class CategorySerializer(serializers.ModelSerializer):
+    sub_categories = serializers.SerializerMethodField()
+
     class Meta:
         model = Category
-        fields = '__all__'
+        fields = ('id', 'name', 'slug', 'is_sub', 'sub_categories')
 
-class PropertySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Property
-        fields = '__all__'
+    def get_sub_categories(self, obj):
+        return CategorySerializer(obj.scategory.all(), many=True).data
+
 
 class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = '__all__'
+        class Meta:
+            model = Product
+            fields = (
+                'id', 'name', 'slug', 'image', 'description', 'price_no_discount', 'discount', 'price', 'is_available')
+
+
+class ProductDetailSerializer(serializers.ModelSerializer):
+        category = serializers.StringRelatedField(read_only=True, many=True)
+        properties = serializers.StringRelatedField(read_only=True, many=True)
+        comments = serializers.SerializerMethodField()
+
+        class Meta:
+            model = Product
+            fields = (
+            'id', 'name', 'category', 'properties', 'slug', 'image', 'description',
+            'price_no_discount', 'discount', 'price', 'is_available', 'comments')
+
+        def get_comments(self, obj):
+            return CommentSerializer(obj.pcomments.all(), many=True).data
+
 
 class CommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Comment
-        fields = '__all__'
+        customer = serializers.StringRelatedField(read_only=True)
+        product = serializers.StringRelatedField(read_only=True)
+
+        class Meta:
+            model = Comment
+            fields = ('id', 'product', 'customer', 'title', 'body', 'created')
