8fb0e1811f9f043afcc01dea4ada6f343cf644be
VahidM93 <vahid9068@gmail.com>
2023-08-03 00:14:24 +0330

    docs: message

--- Jewelry_Online_Shop/orders/serializers.py
+++ Jewelry_Online_Shop/orders/serializers.py
@@ -1,20 +1,43 @@
 from rest_framework import serializers
-from .models import Order, OrderItem, Coupon
 
-class CouponSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Coupon
-        fields = '__all__'
+from customers.models import Customer
+from .models import Order, OrderItem
 
 class OrderItemSerializer(serializers.ModelSerializer):
+    product = serializers.StringRelatedField(read_only=True)
+
     class Meta:
         model = OrderItem
-        fields = '__all__'
+        fields = ('product', 'price', 'quantity')
+
 
 class OrderSerializer(serializers.ModelSerializer):
-    items = OrderItemSerializer(many=True, read_only=True)
-    coupon = CouponSerializer(read_only=True)
+    customer = serializers.StringRelatedField(read_only=True)
+    items = serializers.SerializerMethodField()
+    code = serializers.CharField(required=False)
 
     class Meta:
         model = Order
-        fields = '__all__'
+        fields = (
+            "id", "customer", "is_paid", "discount", "city", "body", "postal_code", "phone_number", "status", "coupon",
+            "code", "transaction_code", "items",
+        )
+
+    def get_items(self, obj):
+        items = obj.items.all()
+        return OrderItemSerializer(instance=items, many=True).data
+
+class CustomerUnpaidOrdersSerializer(serializers.ModelSerializer):
+    unpaid_orders = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Customer
+        fields = ('unpaid_orders',)
+
+    def get_unpaid_orders(self, obj):
+        if obj:
+            unpaid_orders = obj.orders.filter(is_paid=False)
+            result = OrderSerializer(unpaid_orders, many=True).data
+        else:
+            result = {'unpaid_orders': None}
+        return result
