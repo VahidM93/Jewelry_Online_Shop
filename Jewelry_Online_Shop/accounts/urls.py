f8fd91464b458e1e433cf8401a143ada26c0c468
VahidM93 <vahid9068@gmail.com>
2023-08-25 02:21:08 +0330

    update url with token

--- Jewelry_Online_Shop/accounts/urls.py
+++ Jewelry_Online_Shop/accounts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from rest_framework.authtoken import views as auth_token
+
 #namespace in main url should be equal to app_name
 app_name = 'accounts'
 urlpatterns = [
@@ -13,6 +15,19 @@ urlpatterns = [
     path('reset/done/', views.UserPasswordResetDoneView.as_view(), name='password_reset_done'),
     # path('confirm/<uidb64>/<token>/', views.UserPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     # path('confirm/complete/', views.UserPasswordResetCompleteView.as_view(), name='password_reset_complete'),
-
+    path('api-token-auth/', auth_token.obtain_auth_token)
 
 ]
+
+#simpleJWT
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,
+#     TokenRefreshView,
+# )
+
+# urlpatterns = [
+#     ...
+#     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+#     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+#     ...
+# ]
