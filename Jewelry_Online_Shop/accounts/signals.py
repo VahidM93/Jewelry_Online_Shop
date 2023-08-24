bd1d6757f0c5af6af0c37bfcbf80e091957d2d59
VahidM93 <vahid9068@gmail.com>
2023-08-25 02:19:48 +0330

    update signals

--- Jewelry_Online_Shop/accounts/signals.py
+++ Jewelry_Online_Shop/accounts/signals.py
@@ -1,10 +1,14 @@
+from django.conf import settings
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import Account
 from customers.models import Customer
-
+from rest_framework.authtoken.models import Token
 
 @receiver(post_save, sender=Account)
 def create_customer(sender, **kwargs):
     if kwargs['created']:
         Customer.objects.create(user=kwargs['instance'])
+        
+for user in Account.objects.all():
+    Token.objects.get_or_create(user=user)    
